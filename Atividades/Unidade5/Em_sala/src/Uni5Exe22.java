8530e56ef4ae8e865027f0e55d39789f898289e7
silvadavitor <silvadavitor999@gmail.com>
2024-05-13 21:09:24 -0300

    fix Exe22

--- Atividades/Unidade5/Em_sala/src/Uni5Exe22.java
+++ Atividades/Unidade5/Em_sala/src/Uni5Exe22.java
@@ -5,15 +5,16 @@ public class Uni5Exe22 {
         Scanner scanner = new Scanner(System.in);
         
         double salario = 2000;
-        double aumento = 0.15;
+        double aumento = 0.015;
         int data = 1996;
 
         while ( data <= 2024) {
             salario = salario + (salario*aumento);
+            System.out.println("A porcentagem é = " + (aumento*100) + "% e o sálario foi " + salario);
+
             aumento = aumento*2;  
             data++;
 
-        
         }
         System.out.println();
         System.out.println("Salario final = R$ " + aumento + " atualmente em 2024"); 
---
+++ Atividades/Unidade5/Em_sala/src/Uni5Exe25.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Uni5Exe25 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Deseja informar o peso do peixe: s (SIM) / n (NÃO)?”.");
+        char resp = scanner.next().charAt(0);
+        
+        System.out.println("Qual o limite diário em kg?");
+        int limite = scanner.nextInt();
+
+        double total = 0;
+        while ( resp != 'n') {
+            System.out.println("Qual o peso desse peixe em gramas?");
+            double peso = scanner.nextDouble();
+            peso = peso/1000;
+            System.out.println();
+            total += peso;
+            
+            if (total > limite){
+                System.out.println("Excedeu o limite diário");
+                resp = 'n';
+            }
+            else{
+                System.out.println("Total até agora é de " + total + " kg");
+                System.out.println();
+                System.out.println("Deseja informar o peso de mais um peixe: s (SIM) / n (NÃO)?");
+                resp = scanner.next().charAt(0);
+            }
+        
+        }
+        if (total > limite) {
+            System.out.println("No final o total foi de " + limite + " kg");
+        }
+        else{
+            System.out.println("No final o total foi de " + total + " kg");
+        }
+        scanner.close();
+    }
+}
