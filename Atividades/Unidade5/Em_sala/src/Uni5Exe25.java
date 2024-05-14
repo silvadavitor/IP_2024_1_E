dfbf09f4bec5d146fa0a57847867be6e4f336fd1
silvadavitor <silvadavitor999@gmail.com>
2024-05-13 21:28:10 -0300

    Uni5Exe25

--- Atividades/Unidade5/Em_sala/src/Uni5Exe25.java
+++ Atividades/Unidade5/Em_sala/src/Uni5Exe25.java
@@ -4,37 +4,31 @@ public class Uni5Exe25 {
 
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Deseja informar o peso do peixe: s (SIM) / n (NÃO)?”.");
-        char resp = scanner.next().charAt(0);
         
-        System.out.println("Qual o limite diário em kg?");
-        int limite = scanner.nextInt();
-
-        double total = 0;
-        while ( resp != 'n') {
-            System.out.println("Qual o peso desse peixe em gramas?");
-            double peso = scanner.nextDouble();
-            peso = peso/1000;
-            System.out.println();
-            total += peso;
+        
+        int pontos = 0;
+        int D = 0;
+        int E = 0;
+        while ((pontos != 21 && ((D-E ) < 2 || (E-D) < 2 )) || (pontos > 21 && ((D-E ) < 2 || (E-D) < 2 ))) {
+            System.out.println("Quem fez o ponto: E (esquerda) ou D (direita)");
+            char resp = scanner.next().charAt(0);
             
-            if (total > limite){
-                System.out.println("Excedeu o limite diário");
-                resp = 'n';
+            if (resp == 'D'){
+                D++;
+                pontos++;
             }
+
             else{
-                System.out.println("Total até agora é de " + total + " kg");
-                System.out.println();
-                System.out.println("Deseja informar o peso de mais um peixe: s (SIM) / n (NÃO)?");
-                resp = scanner.next().charAt(0);
+               E++;
+               pontos++;
             }
         
         }
-        if (total > limite) {
-            System.out.println("No final o total foi de " + limite + " kg");
+        if (D > E) {
+            System.out.println("Direito GANHOU com " + D + " pontos");
         }
         else{
-            System.out.println("No final o total foi de " + total + " kg");
+            System.out.println("Esquerda GANHOU com " + E + " pontos");
         }
         scanner.close();
     }
